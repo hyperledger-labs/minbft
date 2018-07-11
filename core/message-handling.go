c8fd59f670105178bbcfa42aa0d6c0b7954e6098
Sergey Fedorov <sergey.fedorov@neclab.eu>
2018-07-11 16:47:39 +0200

    core: Do not use channel to order messages for UI assignment
    Incoming message handlers need to make sure that generated messages
    get UI assigned in the same order as corresponding incoming messages.
    Otherwise a replica could assign UIs to its generated messages in
    reverse order relative to the corresponding incoming messages. This
    could confuse other replicas because they will always process messages
    in the order of UI assigned. But they also have to process any
    messages extracted from the incoming messages. Moreover, messages
    generated by a replica include the corresponding incoming message,
    directly or indirectly. Thus, we need to ensure that the order in
    which UI gets assigned to the generated messages is consistent with
    the order of the corresponding incoming messages.
    
    A first step to achieve this would be to include the process of
    assigning UI to the generated message and adding it to the log
    directly into the incoming message handler itself rather than doing
    that in a separate goroutine. This will also make it easier to
    implement generation of ViewChange message later because ViewChange
    message should include all previously generated messages with UI. So
    it would need to stop processing of incoming messages and wait until
    all the message handlers in progress are finished and any generated
    messages have got UI assigned and added to the message log. Then it
    will be easy to pick up all those messages from the log.
    
    Signed-off-by: Sergey Fedorov <sergey.fedorov@neclab.eu>

--- core/message-handling.go
+++ core/message-handling.go
@@ -18,6 +18,7 @@ package minbft
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/golang/protobuf/proto"
 
@@ -38,32 +39,29 @@ type messageStreamHandler func(in <-chan []byte, reply chan<- []byte)
 // message hasn't been processed before.
 type messageHandler func(msg interface{}) (reply <-chan interface{}, new bool, err error)
 
-// generatedUIMessageHandler receives generated messages from in
-// stream, assigns a UI to it and arranges it to be delivered to peer
-// replicas.
-type generatedUIMessageHandler func(in <-chan messages.MessageWithUI)
+// generatedUIMessageHandler assigns and attaches a UI to a generated
+// message and arranges it to be delivered to peer replicas. It is
+// safe to invoke concurrently.
+type generatedUIMessageHandler func(msg messages.MessageWithUI)
 
-// uiMessageConsumer receives generated messages with UI attached and
-// arranges them to be delivered to peer replicas.
+// uiMessageConsumer receives a generated message with UI attached and
+// arranges it to be delivered to peer replicas.
 type uiMessageConsumer func(msg messages.MessageWithUI)
 
 // defaultMessageHandler construct a standard messageHandler using id
 // as the current replica ID and the supplied interfaces.
 func defaultMessageHandler(id uint32, log messagelog.MessageLog, config api.Configer, stack Stack) messageHandler {
 	n := config.N()
-	uiMessageChan := make(chan messages.MessageWithUI)
 
 	view := func() uint64 { return 0 } // view change is not implemented
 	clientStates := clientstate.NewProvider()
 	acceptUI := defaultUIAcceptor(stack)
 	collectCommit := defaultCommitCollector(id, clientStates, config, stack)
-	handleGeneratedUIMessages := defaultGeneratedUIMessageHandler(stack, log)
+	handleGeneratedUIMessage := defaultGeneratedUIMessageHandler(stack, log)
 
-	handleRequest := defaultRequestHandler(id, n, view, stack, clientStates, uiMessageChan)
-	handlePrepare := makePrepareHandler(id, n, view, acceptUI, handleRequest, collectCommit, uiMessageChan)
+	handleRequest := defaultRequestHandler(id, n, view, stack, clientStates, handleGeneratedUIMessage)
+	handlePrepare := makePrepareHandler(id, n, view, acceptUI, handleRequest, collectCommit, handleGeneratedUIMessage)
 	handleCommit := makeCommitHandler(id, n, view, acceptUI, handlePrepare, collectCommit)
-
-	go handleGeneratedUIMessages(uiMessageChan)
 
 	return makeMessageHandler(handleRequest, handlePrepare, handleCommit)
 }
@@ -149,11 +147,14 @@ func makeMessageHandler(handleRequest requestHandler, handlePrepare prepareHandl
 // makeGeneratedUIMessageHandler constructs generatedUIMessageHandler
 // using the supplied abstractions.
 func makeGeneratedUIMessageHandler(assignUI uiAssigner, consume uiMessageConsumer) generatedUIMessageHandler {
-	return func(in <-chan messages.MessageWithUI) {
-		for msg := range in {
-			assignUI(msg)
-			consume(msg)
-		}
+	var lock sync.Mutex
+
+	return func(msg messages.MessageWithUI) {
+		lock.Lock()
+		defer lock.Unlock()
+
+		assignUI(msg)
+		consume(msg)
 	}
 }
 
