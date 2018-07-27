908bfcc116854ae117e0dda2374845c385ccc776
Sergey Fedorov <sergey.fedorov@neclab.eu>
2018-07-27 19:47:47 +0200

    core: Change handling of locally generated messages with UI
    Messages generated locally with UI attached can be received as part of
    some other message from peer replicas. The embedded messages should
    normally be extracted and handled using the same generic message
    handlers as used to handle incoming messages. When handling a message
    produced by the replica itself, there is no need to process the
    message, since it must have already been processed when it was
    generated. There is also no need to check the UI counter value.
    However, the UI attached to the message has to be verified. If the UI
    is not valid, then the enclosing message referring to the invalid
    message is also deemed invalid and should not be processed.
    
    Signed-off-by: Sergey Fedorov <sergey.fedorov@neclab.eu>

--- core/prepare.go
+++ core/prepare.go
@@ -44,10 +44,14 @@ func makePrepareHandler(id, n uint32, view viewProvider, verifyUI uiVerifier, ca
 			return false, fmt.Errorf("UI not valid: %s", err)
 		}
 
-		new = captureUI(replicaID, ui)
-		if new {
-			defer releaseUI(replicaID, ui)
+		if replicaID == id {
+			return false, nil
 		}
+
+		if new = captureUI(replicaID, ui); !new {
+			return false, nil
+		}
+		defer releaseUI(replicaID, ui)
 
 		currentView := view()
 
@@ -61,9 +65,6 @@ func makePrepareHandler(id, n uint32, view viewProvider, verifyUI uiVerifier, ca
 
 		if _, _, err = handleRequest(prepare.Msg.Request, true); err != nil {
 			return false, fmt.Errorf("Failed to process request: %s", err)
-		}
-		if !new || isPrimary(currentView, id, n) {
-			return false, nil
 		}
 
 		commit := &messages.Commit{
