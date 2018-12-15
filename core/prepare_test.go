eecb2d98b126a1da2af889af5316f07186a08487
Sergey Fedorov <sergey.fedorov@neclab.eu>
2018-12-15 22:55:35 +0100

    core: Simplify testing of Prepare processor
    This test is over-complicated, because it combines testing with own
    Prepare message, as well as with one for a different view. Simplify
    the test by setting an expectation on viewProvider for each invocation
    of prepareProcessor individually.
    
    Signed-off-by: Sergey Fedorov <sergey.fedorov@neclab.eu>

--- core/prepare_test.go
+++ core/prepare_test.go
@@ -85,78 +85,35 @@ func TestMakePrepareValidator(t *testing.T) {
 }
 
 func TestMakePrepareProcessor(t *testing.T) {
-	t.Run("Primary", testMakePrepareProcessorPrimary)
-	t.Run("Backup", testMakePrepareProcessorBackup)
-}
-
-func testMakePrepareProcessorPrimary(t *testing.T) {
 	mock := new(testifymock.Mock)
 	defer mock.AssertExpectations(t)
 
 	n := randN()
 	view := randView()
-	id := primaryID(n, view)
-	process := setupMakePrepareProcessorMock(mock, id, view)
-
-	request := &messages.Request{
-		Msg: &messages.Request_M{
-			ClientId: rand.Uint32(),
-		},
-	}
-	makePrepareMsg := func(view uint64, id uint32) *messages.Prepare {
-		return &messages.Prepare{
-			Msg: &messages.Prepare_M{
-				View:      view,
-				ReplicaId: id,
-				Request:   request,
-			},
-		}
-	}
-
-	otherPrimary := randOtherReplicaID(id, n)
-	otherView := viewForPrimary(n, otherPrimary)
-	prepare := makePrepareMsg(otherView, otherPrimary)
-
-	mock.On("requestProcessor", request).Return(false, nil).Once()
-	mock.On("uiCapturer", prepare).Return(true, nil).Once()
-	mock.On("uiReleaser", prepare).Once()
-	_, err := process(prepare)
-	assert.Error(t, err, "Prepare for different view")
-
-	mock.On("requestProcessor", request).Return(false, fmt.Errorf("Invalid request")).Once()
-	_, err = process(prepare)
-	assert.Error(t, err, "Invalid request")
-
-	prepare = makePrepareMsg(view, id)
-
-	new, err := process(prepare)
-	assert.NoError(t, err)
-	assert.False(t, new, "Own Prepare")
-}
-
-func testMakePrepareProcessorBackup(t *testing.T) {
-	mock := new(testifymock.Mock)
-	defer mock.AssertExpectations(t)
-
-	n := randN()
-	view := randView()
+	otherView := randOtherView(view)
 	primary := primaryID(n, view)
 	id := randOtherReplicaID(primary, n)
-	process := setupMakePrepareProcessorMock(mock, id, view)
+
+	process := setupMakePrepareProcessorMock(mock, id)
 
 	request := &messages.Request{
 		Msg: &messages.Request_M{
-			ClientId: rand.Uint32(),
+			Seq: rand.Uint64(),
 		},
 	}
-	makePrepareMsg := func(view uint64, id uint32) *messages.Prepare {
-		return &messages.Prepare{
-			Msg: &messages.Prepare_M{
-				View:      view,
-				ReplicaId: id,
-				Request:   request,
-			},
-		}
+	prepare := &messages.Prepare{
+		Msg: &messages.Prepare_M{
+			View:      view,
+			ReplicaId: primary,
+			Request:   request,
+		},
+	}
+	ownPrepare := &messages.Prepare{
+		Msg: &messages.Prepare_M{
+			View:      viewForPrimary(n, id),
+			ReplicaId: id,
+			Request:   request,
+		},
 	}
 	commit := &messages.Commit{
 		Msg: &messages.Commit_M{
@@ -167,44 +124,46 @@ func testMakePrepareProcessorBackup(t *testing.T) {
 		},
 	}
 
-	otherPrimary := randOtherReplicaID(id, n)
-	otherView := viewForPrimary(n, otherPrimary)
-	prepare := makePrepareMsg(otherView, otherPrimary)
+	new, err := process(ownPrepare)
+	assert.False(t, new)
+	assert.NoError(t, err, "Own message")
 
-	mock.On("requestProcessor", request).Return(false, nil).Once()
-	mock.On("uiCapturer", prepare).Return(true, nil).Once()
-	mock.On("uiReleaser", prepare).Once()
-	_, err := process(prepare)
-	assert.Error(t, err, "Prepare for different view")
-
-	prepare = makePrepareMsg(view, primary)
-
-	mock.On("requestProcessor", request).Return(false, fmt.Errorf("Invalid request")).Once()
+	mock.On("requestProcessor", request).Return(false, fmt.Errorf("Error")).Once()
 	_, err = process(prepare)
-	assert.Error(t, err, "Invalid Request")
+	assert.Error(t, err, "Failed to process Request")
 
 	mock.On("requestProcessor", request).Return(false, nil).Once()
-	mock.On("uiCapturer", prepare).Return(false, nil).Once()
-	new, err := process(prepare)
+	mock.On("uiCapturer", prepare).Return(false).Once()
+	new, err = process(prepare)
 	assert.NoError(t, err)
 	assert.False(t, new, "UI already processed")
 
 	mock.On("requestProcessor", request).Return(false, nil).Once()
-	mock.On("uiCapturer", prepare).Return(true, nil).Once()
+	mock.On("uiCapturer", prepare).Return(true).Once()
+	mock.On("viewProvider").Return(otherView).Once()
+	mock.On("uiReleaser", prepare).Once()
+	_, err = process(prepare)
+	assert.Error(t, err, "Prepare for different view")
+
+	mock.On("requestProcessor", request).Return(false, nil).Once()
+	mock.On("uiCapturer", prepare).Return(true).Once()
+	mock.On("viewProvider").Return(view).Once()
 	mock.On("requestSeqPreparer", request).Return(false).Once()
 	mock.On("uiReleaser", prepare).Once()
 	_, err = process(prepare)
-	assert.Error(t, err, "Request already prepared")
+	assert.Error(t, err, "Request ID already prepared")
 
 	mock.On("requestProcessor", request).Return(false, nil).Once()
-	mock.On("uiCapturer", prepare).Return(true, nil).Once()
+	mock.On("uiCapturer", prepare).Return(true).Once()
+	mock.On("viewProvider").Return(view).Once()
 	mock.On("requestSeqPreparer", request).Return(true).Once()
-	mock.On("commitCollector", commit).Return(fmt.Errorf("Duplicated commit detected")).Once()
+	mock.On("commitCollector", commit).Return(fmt.Errorf("Error")).Once()
 	mock.On("uiReleaser", prepare).Once()
-	assert.Panics(t, func() { process(prepare) }, "Failed collecting own Commit")
+	assert.Panics(t, func() { process(prepare) }, "Failed to collect own Commit")
 
 	mock.On("requestProcessor", request).Return(false, nil).Once()
-	mock.On("uiCapturer", prepare).Return(true, nil).Once()
+	mock.On("uiCapturer", prepare).Return(true).Once()
+	mock.On("viewProvider").Return(view).Once()
 	mock.On("requestSeqPreparer", request).Return(true).Once()
 	mock.On("commitCollector", commit).Return(nil).Once()
 	mock.On("generatedUIMessageProcessor", commit).Once()
@@ -214,7 +173,7 @@ func testMakePrepareProcessorBackup(t *testing.T) {
 	assert.True(t, new)
 }
 
-func setupMakePrepareProcessorMock(mock *testifymock.Mock, id uint32, view uint64) prepareProcessor {
+func setupMakePrepareProcessorMock(mock *testifymock.Mock, id uint32) prepareProcessor {
 	provideView := func() uint64 {
 		args := mock.MethodCalled("viewProvider")
 		return args.Get(0).(uint64)
@@ -240,6 +199,5 @@ func setupMakePrepareProcessorMock(mock *testifymock.Mock, id uint32, view uint6
 	handleGeneratedUIMessage := func(msg messages.MessageWithUI) {
 		mock.MethodCalled("generatedUIMessageProcessor", msg)
 	}
-	mock.On("viewProvider").Return(view)
 	return makePrepareProcessor(id, provideView, captureUI, prepareRequestSeq, processRequest, collectCommit, handleGeneratedUIMessage)
 }
