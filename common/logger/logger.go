494bcf6653fe76b17a9234f24f94b35365bffb09
Konstantin Munichev <Konstantin.Munichev@neclab.eu>
2020-10-23 13:54:39 +0200

    Introduce Logger interface
    Logger interface is supposed to be an universal logger dependency.
    The main goal is to make possible to use both internal logger
    implementation as well as external one given from the outside.
    
    Signed-off-by: Konstantin Munichev <konstantin.munichev@neclab.eu>

---
+++ common/logger/logger.go
@@ -0,0 +1,32 @@
+// Copyright © 2020 NEC Laboratories Europe GmbH.
+//
+// Authors: Konstantin Munichev <konstantin.munichev@neclab.eu>
+//
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package logger
+
+// Logger provides a basic functionality for any logger implementation
+type Logger interface {
+	Debug(args ...interface{})
+	Debugf(template string, args ...interface{})
+	Info(args ...interface{})
+	Infof(template string, args ...interface{})
+	Warning(args ...interface{})
+	Warningf(template string, args ...interface{})
+	Error(args ...interface{})
+	Errorf(template string, args ...interface{})
+	Fatal(args ...interface{})
+	Fatalf(template string, args ...interface{})
+}
